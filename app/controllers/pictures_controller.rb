9c779b99d17989f1c3057d79c3eca969d29d442b
Harrison Taylor <h.taylor@project89.org>
2015-04-15 15:53:32 -0400

    Added image submission form

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,4 +1,5 @@
 class PicturesController < ApplicationController
+
   def index
     @pictures = Picture.all
   end
@@ -6,4 +7,12 @@ class PicturesController < ApplicationController
   def show
     @picture = Picture.find(params[:id])
   end
+
+  def new
+  end
+
+  def create
+    render :text => "Saving a picture. URL: #{params[:url]}, Title: #{params[:title]}, Artist: #{params[:artist]}"
+  end
+
 end
